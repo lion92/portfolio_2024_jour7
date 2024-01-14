7453377c5f8e5b1e3d38d48a1c5930473a444545
lion92 <38532569+lion92@users.noreply.github.com>
2024-01-14 13:36:15 +0100

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Helloword from "./Component/Helloword";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Root = () => (
+    <Router>
+        <Switch>
+            <Route exact path="/" component={Helloword}/>
+
+        </Switch>
+    </Router>
+)
+
+ReactDOM.render(<Root/>, document.getElementById('root'));
+
+
+
+
