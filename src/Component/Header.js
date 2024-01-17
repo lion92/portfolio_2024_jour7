6be3160e326dab53d51818bea0a4bd23c2ab5e5e
lion92 <38532569+lion92@users.noreply.github.com>
2024-01-17 19:55:51 +0100

    jour 5 menu refais

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,40 +1,103 @@
-import React from 'react';
-import './css/header.css'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Tooltip from '@mui/material/Tooltip';
+import PersonAdd from '@mui/icons-material/PersonAdd';
+import Settings from '@mui/icons-material/Settings';
+import Logout from '@mui/icons-material/Logout';
 import {NavLink} from "react-router-dom";
 
-
-const Header = () => {
-
+export default function AccountMenu() {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
     return (
-        <>
-            <header>
-                <nav>
-                    <div className="logo">
-                        <NavLink to={"/"}>
-                            <h5>Kriss CLOTILDE</h5>
-                        </NavLink>
-                    </div>
-                    <ul className="menu">
-                        <li><NavLink to={"/About"}>
-                            <li>A Propos</li>
-                        </NavLink></li>
-                        <li><NavLink to={"/Projets"}>
-                            <li>Projets</li>
-                        </NavLink></li>
-                        <li>
-                            <NavLink to={"/Parcours"}>
-                                <li>Parcours</li>
-                            </NavLink></li>
-                        <li><NavLink to={"/Contact"}>
-                            <li>Contact</li>
-                        </NavLink></li>
-                    </ul>
+        <React.Fragment>
+            <Box sx={{display: 'flex', alignItems: 'center', textAlign: 'center'}}>
+                <Tooltip title="Account settings">
+                    <IconButton
+                        onClick={handleClick}
+                        size="small"
+                        sx={{ml: 2}}
+                        aria-controls={open ? 'account-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}
+                    >
+                        <Avatar sx={{width: 100, height: 100}}>Menu</Avatar>
+                    </IconButton>
+                </Tooltip>
+            </Box>
+            <Menu
+                anchorEl={anchorEl}
+                id="account-menu"
+                open={open}
+                onClose={handleClose}
+                onClick={handleClose}
+                PaperProps={{
+                    elevation: 0,
+                    sx: {
+                        overflow: 'visible',
+                        filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                        mt: 1.5,
+                        '& .MuiAvatar-root': {
+                            width: 32,
+                            height: 32,
+                            ml: -0.5,
+                            mr: 1,
+                        },
+                        '&::before': {
+                            content: '""',
+                            display: 'block',
+                            position: 'absolute',
+                            top: 0,
+                            right: 14,
+                            width: 10,
+                            height: 10,
+                            bgcolor: 'background.paper',
+                            transform: 'translateY(-50%) rotate(45deg)',
+                            zIndex: 0,
+                        },
+                    },
+                }}
+                transformOrigin={{horizontal: 'right', vertical: 'top'}}
+                anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
+            >
+                <MenuItem>
+                    <NavLink to={"/"}>
+                        <li>Bienvenue</li>
+                    </NavLink>
+                </MenuItem>
+                <MenuItem>
+                    <NavLink to={"/About"}>
+                        <li>A Propos</li>
+                    </NavLink>
+                </MenuItem>
+                <MenuItem>
+                    <NavLink to={"/Projets"}>Projets</NavLink>
+                </MenuItem>
 
-                </nav>
-
-            </header>
-
-        </>
-    )
+                <MenuItem>
+                    <NavLink to={"/Parcours"}>
+                        <li>Parcours</li>
+                    </NavLink>
+                </MenuItem>
+                <MenuItem>
+                    <NavLink to={"/Contact"}>
+                        <li>Contact</li>
+                    </NavLink>
+                </MenuItem>
+            </Menu>
+        </React.Fragment>
+    );
 }
-export default Header;
