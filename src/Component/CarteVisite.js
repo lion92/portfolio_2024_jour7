10a99e0f3735cb81fa7c91b2da881b401d00da4b
lion92 <38532569+lion92@users.noreply.github.com>
2024-01-17 13:54:17 +0100

    jour5 de code amélioré carte de visite

--- src/Component/CarteVisite.js
+++ src/Component/CarteVisite.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import './css/contact.css'
-import Header from "./Header";
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
 import Footer from "./Footer";
+import Header from "./Header";
 
-const CarteVisite = () => {
+export default function CarteVisite() {
     return (
         <div>
             <Header></Header>
+            <div  className="container">
+        <Card style={{padding:"20px"}} sx={{ maxWidth: 450 }}>
+            <CardMedia
+                sx={{ height: 200 }}
+                image="https://projet.krissclotilde.com/asset/avatar-gratuit.png"
+                title="Moi"
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    Developpeur
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    <a href="https://www.linkedin.com/in/kriss-clotilde/"
+                       target="_blank"
+                    >Linkdin</a>
+                </Typography>
+            </CardContent>
+            <CardActions>
 
-
+            </CardActions>
+        </Card>
+            </div>
             <Footer></Footer>
         </div>
     );
-};
-
-export default CarteVisite;
+}
