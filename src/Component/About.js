bbab5adff98c6e1e4ab99bb94619667be75b337f
lion92 <38532569+lion92@users.noreply.github.com>
2024-01-18 07:45:22 +0100

    jour 6 refais progress bar

--- src/Component/About.js
+++ src/Component/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './css/about.css'
 import Header from "./Header";
+import {LinearProgress} from "@mui/material";
 
 
 const About = () => {
@@ -17,44 +18,31 @@ const About = () => {
                         J'aime le craft et les bonnes pratiques de programmations. <br/>
                         Je vise les étoiles pour au moins décrocher la lune.</p>
 
-                    <div className=" wow fadeInUp container-skillbar" data-wow-delay="0.2s">
-                        <div className="skillbar clearfix " data-percent="90%">
-                            <div className="skillbar-title"><span>Java</span></div>
-                            <div className="skillbar-bar"></div>
-                            <div className="skill-bar-percent">90%</div>
-                        </div>
 
-
-                        <div className="skillbar clearfix " data-percent="80%">
-                            <div className="skillbar-title"><span>Javascript</span></div>
-                            <div className="skillbar-bar"></div>
-                            <div className="skill-bar-percent">80%</div>
-                        </div>
-
-
-                        <div className="skillbar clearfix " data-percent="75%">
-                            <div className="skillbar-title"><span>React</span></div>
-                            <div className="skillbar-bar"></div>
-                            <div className="skill-bar-percent">60%</div>
-                        </div>
-
-
-                        <div className="skillbar clearfix " data-percent="45%">
-                            <div className="skillbar-title"><span>Angular</span></div>
-                            <div className="skillbar-bar"></div>
-                            <div className="skill-bar-percent">60%</div>
-                        </div>
-
-                        <div className="skillbar clearfix " data-percent="50%">
-                            <div className="skillbar-title"><span>Spring</span></div>
-                            <div className="skillbar-bar"></div>
-                            <div className="skill-bar-percent">60%</div>
-                        </div>
-
-                    </div>
                 </div>
-            </div>
+                <h2>Java </h2>
+                <LinearProgress style={{width: '40vw', margin: '50px auto'}}
+                                variant="determinate" value={90}/>
+                <h2>Spring </h2>
+                <LinearProgress
+                    style={{width: '40vw', margin: '50px auto'}}
+                    variant="determinate" value={60}/>
 
+                <h2>Javascrit </h2>
+                <LinearProgress
+                    style={{width: '40vw', margin: '50px auto'}}
+                    variant="determinate" value={80}/>
+                <h2>React </h2>
+                <LinearProgress style={{width: '40vw', margin: '50px auto'}}
+                                variant="determinate" value={60}/>
+                <h2>Angular </h2>
+                <LinearProgress
+                    style={{width: '40vw', margin: '50px auto'}}
+                    variant="determinate" value={60}/>
+
+
+
+            </div>
         </div>
     );
 };
